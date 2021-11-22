e3a5251d1503101e4f2eab4bd724928512497cb6
elodie baeza <elodie.baeza@gmail.com>
2021-11-22 14:20:23 +0100

    Replace class by className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,22 @@ import logo from 'assets/argentBankLogo.png'
 export default function Header(){
     return(
         <React.Fragment>
-            <nav class="main-nav">
-                <a class="main-nav-logo" href="./">
+            <nav className="main-nav">
+                <a className="main-nav-logo" href="./">
                     <img
-                    class="main-nav-logo-image"
+                    className="main-nav-logo-image"
                     src={logo}
                     alt="Argent Bank Logo"
                     />
-                    <h1 class="sr-only">Argent Bank</h1>
+                    <h1 className="sr-only">Argent Bank</h1>
                 </a>
                 <div>
-                    <a class="main-nav-item" href="./login">
-                        <i class="fa fa-user-circle"></i>
+                    <a className="main-nav-item" href="./login">
+                        <i className="fa fa-user-circle"></i>
                         Sign In
                     </a>
-                    <a class="main-nav-item" href="./">
-                        <i class="fa fa-sign-out"></i>
+                    <a className="main-nav-item" href="./">
+                        <i className="fa fa-sign-out"></i>
                         Sign Out
                     </a>
                 </div>
