1994800853b2644515396e3bc48b2e635cdbe784
elodie baeza <elodie.baeza@gmail.com>
2021-11-22 20:52:20 +0100

    User can login to the system

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
 import Login from 'pages/Login';
 import Profile from 'pages/Profile';
 import './App.css';
-import React from "react"
-import { Provider } from 'react-redux';
-import store from './utils/store';
+import React, { useState } from "react"
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 
 export default function App() {
+  const [token, updateToken] = useState()
+
   return (
-    <Provider store={store}>
-      <React.Fragment>
-        <BrowserRouter>
-          <Header userText=''/>
-          <Routes>
-            <Route exact path='/'
-            element={ <Home /> }/>
-            <Route exact path='/login'
-            element={ <Login /> }/>
-            <Route exact path='/profile'
-            element={ <Profile /> }/>
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </React.Fragment>
-    </Provider>
+    <React.Fragment>
+      <BrowserRouter>
+        <Header userText=''/>
+        <Routes>
+          <Route exact path='/'
+          element={ <Home /> }/>
+          <Route exact path='/login'
+          element={ <Login token={token} updateToken={updateToken}/> }/>
+          <Route exact path='/profile'
+          element={ <Profile token={token} /> }/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,34 +1,93 @@
-import React from "react"
+import apiProvider from "data/apiProvider";
+import React, { useEffect, useState } from "react"
+import { Field, Form } from "react-final-form";
+import { useNavigate } from "react-router";
 
-export default function Login() {
-  return (
-    <React.Fragment>
-      <main class="main bg-dark">
-        <section class="sign-in-content">
-            <i class="fa fa-user-circle sign-in-icon"></i>
-            <h1>Sign In</h1>
-            <form>
-                <div class="input-wrapper">
-                    <label for="username">Username</label
-                    ><input type="text" id="username" />
-                </div>
-                <div class="input-wrapper">
-                    <label for="password">Password</label
-                    ><input type="password" id="password" />
-                </div>
-                <div class="input-remember">
-                    <input type="checkbox" id="remember-me" /><label for="remember-me"
-                    >Remember me</label
-                    >
-                </div>
-                {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
-                <a href="./profile" class="sign-in-button">Sign In</a>
-                {/* <!-- SHOULD BE THE BUTTON BELOW --> */}
-                {/* <!-- <button class="sign-in-button">Sign In</button> --> */}
-                {/* <!--  --> */}
-            </form>
-        </section>
-      </main>
-    </React.Fragment>
-  );
+export default function Profile({token, updateToken}) {
+    const [formData, setFormData] = useState({})
+    const navigate = useNavigate()
+
+    const onSubmit = (values) => {
+        setFormData(values)
+    }
+
+    useEffect(() => {
+        apiProvider.login(formData)
+            .then((response) => {
+                updateToken(response.data.body.token)
+                navigate('/profile')
+                console.log(token)
+            })
+            .catch(error => {
+                console.log(error)
+                throw error
+            })
+      },[formData, navigate, token, updateToken])
+
+    return (
+        <React.Fragment>
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <Form 
+                    onSubmit={onSubmit}
+                    initialValues={formData}
+                    validate={(values) => {
+                        const errors = {};
+                        if (!values.email) {
+                        errors.email = "Required";
+                        }
+                        if (!values.password) {
+                        errors.password = "Required";
+                        }
+                        return errors;    
+                    }}
+                    render={({ handleSubmit, form, submitting, pristine, values }) => (
+                        <form onSubmit={handleSubmit}>
+                            <div className="input-wrapper">
+                                <Field name="email">
+                                  {({ input, meta }) => (
+                                    <div>
+                                      <label htmlFor="username">Username</label>
+                                      <input {...input} type="text" id='username' />
+                                      {(meta.error || meta.submitError) && meta.touched && (
+                                        <span>{meta.error || meta.submitError}</span>
+                                      )}
+                                    </div>
+                                  )}
+                                </Field>
+                            </div>
+                            <div className="input-wrapper">
+                                <Field name="password">
+                                  {({ input, meta }) => (
+                                    <div>
+                                      <label htmlFor="username">Password</label>
+                                      <input {...input} type="password" id='password' />
+                                      {(meta.error || meta.submitError) && meta.touched && (
+                                        <span>{meta.error || meta.submitError}</span>
+                                      )}
+                                    </div>
+                                  )}
+                                </Field>
+                            </div>
+                            <div className="input-wrapper">
+                                <label htmlFor="rememberMe">Remember-me</label>
+                                <Field 
+                                name="rememberMe" 
+                                type="checkbox" 
+                                component='input'
+                                />
+                            </div>
+                            <button 
+                            className="sign-in-button" 
+                            type='submit' 
+                            disabled={submitting}>Sign In</button>
+                        </form>
+                    )}
+                    />
+                </section>
+            </main>                    
+        </React.Fragment>
+    );
 }
