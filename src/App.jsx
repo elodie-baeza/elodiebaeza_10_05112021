1dbb1cb4e95dcbba8bdfe83f733a461a1e699e71
elodie baeza <elodie.baeza@gmail.com>
2021-11-22 14:27:35 +0100

    Debug reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import Login from 'pages/Login';
 import Profile from 'pages/Profile';
 import './App.css';
 import React from "react"
-import { Provider } from 'react-redux';
-import store from './utils/store';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -11,21 +9,19 @@ import Home from 'pages/Home';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <React.Fragment>
-        <BrowserRouter>
-          <Header userText=''/>
-          <Routes>
-            <Route exact path='/'
-            element={ <Home /> }/>
-            <Route exact path='/login'
-            element={ <Login /> }/>
-            <Route exact path='/profile'
-            element={ <Profile /> }/>
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </React.Fragment>
-    </Provider>
+    <React.Fragment>
+      <BrowserRouter>
+        <Header userText=''/>
+        <Routes>
+          <Route exact path='/'
+          element={ <Home /> }/>
+          <Route exact path='/login'
+          element={ <Login /> }/>
+          <Route exact path='/profile'
+          element={ <Profile /> }/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from 'features/login'
 
 const store = configureStore({
     reducer: {
-        
+        login: loginReducer,
     }
 })
 
